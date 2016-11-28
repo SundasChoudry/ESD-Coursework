2150e9c143e3467a8f4ad46922c2c026aa7ee371
Loyson <loyson07@hotmail.co.uk>
2016-11-28 21:46:44 +0000

    Display Username/Password after Registration

--- src/java/controller/Registration.java
+++ src/java/controller/Registration.java
@@ -6,9 +6,12 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -33,29 +36,42 @@ public class Registration extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         JDBCBean bean = (JDBCBean) getServletContext().getAttribute("JDBCBean");
-        //boolean Registration = false;
+        String errorMessage = "";
 
-        String name = request.getParameter("name");
+        String fullName = request.getParameter("fullName");
         //generating username
-        char initial = name.charAt(0);
-        String[] surname = name.split(" ");
-        String username = (initial + "-" + surname[1]).toLowerCase();
-        String address = request.getParameter("address");
-        String dob = request.getParameter("DOB");
-        //DOR to current date in YYYY-MM-DD format
-        SimpleDateFormat sqlDateFormatForRegistration = new SimpleDateFormat("yyyy-MM-dd");
-        String dor = sqlDateFormatForRegistration.format(Calendar.getInstance().getTime());
-        //password in DDMMYY format
-        String password = request.getParameter("DOB").replaceAll("(..)(..)-(..)-(..)", "$4$3$2");
-        float RegistrationFee = 10;
-        String defaultStatus = "APPLIED";
+        char initial = fullName.charAt(0);
+        String[] names = fullName.split(" ");
+        //Check if full name is entered
+        if (names.length < 2) {
+            errorMessage = "<div class=\"error\">\n"
+                    + "  <span class=\"errorMessageBtn\" onclick=\"this.parentElement.style.display='none';\">&times;</span> \n"
+                    + "  <strong>Full Name Required!</strong> \n"
+                    + "</div>";
+            request.setAttribute("ErrorMessage", errorMessage);
+            RequestDispatcher view = request.getRequestDispatcher("/docs/Registration");
+            view.forward(request, response);
+        } else {
+            String username = (initial + "-" + names[1]).toLowerCase();
+            String address = request.getParameter("address");
+            String dob = request.getParameter("DOB");
+            //DOR to current date in YYYY-MM-DD format
+            SimpleDateFormat sqlDateFormatForRegistration = new SimpleDateFormat("yyyy-MM-dd");
+            String dor = sqlDateFormatForRegistration.format(Calendar.getInstance().getTime());
+            //password in DDMMYY format
+            String password = request.getParameter("DOB").replaceAll("(..)(..)-(..)-(..)", "$4$3$2");
+            float RegistrationFee = 10;
+            String defaultStatus = "APPLIED";
 
-        bean.executeSQLUpdate("INSERT INTO `Members`(`id`, `name`, `address`, `dob`, `dor`, `status`, `balance`)"
-                + "VALUES (" + "'" + username + "','" + name + "','" + address + "','" + dob + "','" + dor + "','" + defaultStatus + "'," + RegistrationFee + ")");
-        bean.executeSQLUpdate("INSERT INTO `users`(`id`, `password`, `status`)"
-                + "VALUES (" + "'" + username + "','" + password + "','" + defaultStatus + "'" + ")");
+            bean.executeSQLUpdate("INSERT INTO `Members`(`id`, `name`, `address`, `dob`, `dor`, `status`, `balance`)"
+                    + "VALUES (" + "'" + username + "','" + fullName + "','" + address + "','" + dob + "','" + dor + "','" + defaultStatus + "'," + RegistrationFee + ")");
+            bean.executeSQLUpdate("INSERT INTO `users`(`id`, `password`, `status`)"
+                    + "VALUES (" + "'" + username + "','" + password + "','" + defaultStatus + "'" + ")");
 
-        request.getRequestDispatcher("/docs/RegistrationSuccessful").forward(request, response);
+            request.setAttribute("registeredUsername", username);
+            request.setAttribute("registeredUsernamePassword", password);
+            request.getRequestDispatcher("/docs/RegistrationSuccessful").forward(request, response);
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
--- web/WEB-INF/docs/RegistrationSuccessful.jsp
+++ web/WEB-INF/docs/RegistrationSuccessful.jsp
@@ -3,22 +3,17 @@
     Author     : Loy
 --%>
 
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Registration Successful</title>
-    </head>
-    <body>
-        <div class="main">
-            <div class="content-screen">
-                <div class="title">
-                    <h1>Registration Successful</h1>
-                </div>
-                <a class="btn" href="${pageContext.request.contextPath}/docs/Login">Login Page</a>
-            </div>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<div class="main">
+    <div class="content-screen">
+        <div class="title">
+            <h1>Registration Successful</h1>
+            <p>Welcome <b>${registeredUsername}</b></p>
+            <p>Your Password is <b>${registeredUsernamePassword}</b></p>
         </div>
-    </body>
-</html>
+        <a class="btn" href="${pageContext.request.contextPath}/docs/Login">Login Page</a>
+    </div>
+</div>
+
 
